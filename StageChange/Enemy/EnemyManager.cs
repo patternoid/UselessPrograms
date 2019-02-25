203b8656929f14242fe987a63b80c6e1ac3f4585
Sung Jun yub <joonv123@gmail.com>
2019-02-25 23:36:09 +0900

    Unit 클래스 생성

---
+++ StageChange/Enemy/EnemyManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StageChange.Enemy
+{
+	public class EnemyManager
+	{
+		public EnemyManager()
+		{
+
+		}
+
+		public void Initialize()
+		{
+
+		}
+	}
+}
---
+++ StageChange/GameActor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StageChange
+{
+	public abstract class GameActor
+	{
+		public		float		Hp{ get{ return m_fHp; } set{ m_fHp = value; } }
+		public		float		Power{ get{ return m_fPower; } }
+		public		bool		IsDeath{ get{ return (m_fHp <= 0); }}
+
+		protected	float		m_fHp;
+		protected	float		m_fPower;
+	}
+}
