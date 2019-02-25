702fcaf4becb02f232ffd2f7520fd9da65e37c79
Sung Jun yub <joonv123@gmail.com>
2019-02-25 23:36:21 +0900

    Damage 관련 클래스 생성

---
+++ StageChange/DamageCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StageChange
+{
+	public class DamageCalculator
+	{
+		public DamageCalculator()
+		{
+
+		}
+
+		public void Initialize()
+		{
+
+		}
+
+		public float CaculateDamage( GameActor kAttacker )
+		{
+			return kAttacker.Power;
+		}
+	}
+}
---
+++ StageChange/DamageSender.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StageChange
+{
+	public class DamageSender
+	{
+		private		DamageCalculator	m_kDamageCalculator		= null;
+
+		public DamageSender( DamageCalculator kDamageCalculator )
+		{
+			m_kDamageCalculator		= kDamageCalculator;
+		}
+
+		public void Initialize()
+		{
+
+		}
+
+		public void SendDamage( GameActor kFrom, GameActor kTo )
+		{
+			float	a_fFromPower		= m_kDamageCalculator.CaculateDamage( kFrom );
+			float	a_fToHp				= kTo.Hp;
+			float	a_fDamageResult		= a_fToHp - a_fFromPower;
+			bool	a_bIsDeath			= false;
+
+			kTo.Hp			= a_fDamageResult;  
+			a_bIsDeath		= kTo.IsDeath;
+
+			if( a_bIsDeath == true )
+			{
+				//어떤 역할을 해준다.
+
+			}
+		}
+	}
+}
